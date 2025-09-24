50ff31d2e177222d2b0129a5591fde03236e4a0b
huyperfect <huydkde170020@fpt.edu.vn>
2025-09-24 21:48:08 +0700

    initial commit

---
+++ PAS_APP/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PAS_APP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Signin()
+        {
+            return View();
+        }
+    }
+}
